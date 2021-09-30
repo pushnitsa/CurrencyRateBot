e6626fd1b4cb00cfa42c50f8184406d0f028be28
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-09-30 20:31:05 +0200

    Add home controller

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyRateBot.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
