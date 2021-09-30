45b1f98e4fe90292942de8497e850ee8f7831f23
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-09-30 21:32:58 +0200

    Add controller

---
+++ src/Controllers/CurrencyRateController.cs
@@ -0,0 +1,26 @@
+﻿using CurrencyRateBot.Web.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CurrencyRateBot.Controllers
+{
+    [ApiController]
+    [Route("api/currency")]
+    public class CurrencyRateController : Controller
+    {
+        private readonly CurrencyRateService _currencyRateService;
+
+        public CurrencyRateController(CurrencyRateService currencyRateService)
+        {
+            _currencyRateService = currencyRateService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<decimal>> GetCurrencyRate(string from, string to)
+        {
+            var result = await _currencyRateService.GetCurrencyRateAsync(from, to);
+
+            return Ok(result);
+        }
+    }
+}
