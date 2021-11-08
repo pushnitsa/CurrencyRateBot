0b5955c91d2df2e4a0e66f7ffff50dc59f398338
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-11-08 18:43:47 +0200

    Use modern http client

--- src/CurrencyRateBot.Web/Service/CurrencyRateService.cs
+++ src/CurrencyRateBot.Web/Service/CurrencyRateService.cs
@@ -1,6 +1,5 @@
-using System;
-using System.IO;
-using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
@@ -18,29 +17,17 @@ namespace CurrencyRateBot.Web.Service
 
         public virtual async Task<decimal> GetCurrencyRateAsync(string from, string to)
         {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(_currencyRateServerOptions.Url);
+            var client = new HttpClient();
 
-            httpWebRequest.Method = "POST";
-            httpWebRequest.ContentType = "application/json";
+            var data = new StringContent(
+                $"{{\"method\":\"spotRateHistory\",\"data\":{{\"base\":\"{from}\",\"term\":\"{to}\",\"period\":\"day\"}}}}",
+                Encoding.UTF8, "application/json");
 
-            var requestData = $"{{\"method\":\"spotRateHistory\",\"data\":{{\"base\":\"{from}\",\"term\":\"{to}\",\"period\":\"day\"}}}}";
-            var byteArray = System.Text.Encoding.UTF8.GetBytes(requestData);
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36");
 
-            httpWebRequest.ContentLength = byteArray.Length;
-            httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36";
-            httpWebRequest.Accept = "*/*";
+            var responseData = await client.PostAsync(_currencyRateServerOptions.Url, data);
 
-            await using (var dataStream = httpWebRequest.GetRequestStream())
-            {
-                await dataStream.WriteAsync(byteArray.AsMemory(0, byteArray.Length));
-            }
-
-            var response = await httpWebRequest.GetResponseAsync();
-            await using var stream = response.GetResponseStream();
-            using var reader = new StreamReader(stream);
-            var responseData = await reader.ReadToEndAsync();
-
-            var jsonParsedData = JsonDocument.Parse(responseData);
+            var jsonParsedData = JsonDocument.Parse(await responseData.Content.ReadAsStringAsync());
 
             var result = jsonParsedData
                 .RootElement
